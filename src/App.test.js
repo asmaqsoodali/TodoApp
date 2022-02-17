47914c8fff71faa096c289c905aff0787a28ca1d
Maqsood Ali <ali.sapise07@gmail.com>
2022-02-18 02:15:34 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('app component renders without crashing', () =>{
+   const wrapper = shallow(<App/>);
+   expect(wrapper.exists()).toBe(true);
 });
+
